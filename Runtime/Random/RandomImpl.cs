5e86d3f65e92563d8e87b6396102da457c98dedf
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-05-16 17:59:58 +0900

    Add RandomImpl default constructor

--- Runtime/Random/RandomImpl.cs
+++ Runtime/Random/RandomImpl.cs
@@ -1,25 +1,35 @@
 ﻿// Copyright (c) 2023 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System;
+
 namespace TestHelper.Monkey.Random
 {
     /// <summary>
-    /// Reference implementation of Random class using <c>System.Random</c>.
+    /// Reference implementation of <c>IRandom</c>, wrapping <c>System.Random</c>.
     /// </summary>
+    /// <seealso cref="System.Random"/>
     public class RandomImpl : IRandom
     {
         private readonly System.Random _random;
         private readonly int _seed;
 
         /// <summary>
-        /// Initializes a new instance of the <c>RandomImpl</c> class with seed value.
+        /// Initializes a new instance of the <c>RandomImpl</c> class,
+        /// using the specified seed value.
         /// </summary>
-        /// <param name="seed">random seed</param>
+        /// <param name="seed">A number used to calculate a starting value for the pseudo-random number sequence. If a negative number is specified, the absolute value of the number is used.</param>
         public RandomImpl(int seed)
         {
             _random = new System.Random(seed);
             _seed = seed;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>RandomImpl</c> class,
+        /// using <c>Environment.TickCount</c> to seed value.
+        /// </summary>
+        public RandomImpl() : this(Environment.TickCount) { }
 
         /// <inheritdoc />
         public int Next(int maxValue)
@@ -30,7 +40,7 @@ namespace TestHelper.Monkey.Random
         /// <inheritdoc />
         public override string ToString()
         {
-            return $"System.Random, seed={_seed}";
+            return $"wrapping System.Random, seed={_seed}";
         }
     }
 }
