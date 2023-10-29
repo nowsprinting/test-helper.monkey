7fcb932441f1e8954a7e0e85dcf1c37b2d6f976a
Kuniwak <yuki.kokubun@dena.com>
2023-10-29 17:48:36 +0900

    Remove unnecessary codes

--- Tests/Runtime/Annotations/PositionAnnotationTest.cs
+++ Tests/Runtime/Annotations/PositionAnnotationTest.cs
@@ -1,13 +1,11 @@
 // Copyright (c) 2023 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System.Collections;
 using System.Linq;
-using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
 using NUnit.Framework;
 using TestHelper.Attributes;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace TestHelper.Monkey.Annotations
@@ -17,27 +15,26 @@ namespace TestHelper.Monkey.Annotations
     public class PositionAnnotationTest
     {
         private const string TestScene = "Packages/com.nowsprinting.test-helper.monkey/Tests/Scenes/Annotations.unity";
-        
-        [SetUp]
-        public async Task SetUp()
-        {
-#if UNITY_EDITOR
-            await UnityEditor.SceneManagement.EditorSceneManager.LoadSceneAsyncInPlayMode(
-                "Packages/com.nowsprinting.test-helper.monkey/Tests/Scenes/Annotations.unity",
-                new LoadSceneParameters(LoadSceneMode.Single));
-#endif
-        }
 
-        [TestCase("WorldOffsetAnnotation")]
-        [TestCase("ScreenOffsetAnnotation")]
-        [TestCase("WorldPositionAnnotation")]
-        [TestCase("ScreenPositionAnnotation")]
+        private static object[][] s_TestCases =>
+            new[]
+            {
+                new object[] { "WorldOffsetAnnotation" },
+                new object[] { "ScreenOffsetAnnotation" },
+                new object[] { "WorldPositionAnnotation" },
+                new object[] { "ScreenPositionAnnotation" }
+            };
+
+        [TestCaseSource(nameof(s_TestCases))]
         [LoadScene(TestScene)]
-        public void IsReallyInteractive(string name)
+        [GameViewResolution(640, 480, "VGA")]
+        public IEnumerator IsReallyInteractive(string name)
         {
+            yield return null;
+
             var target = InteractiveComponentCollector.FindInteractiveComponents(false)
                 .First(x => x.gameObject.name == name);
-            
+
             // Without no position annotations, IsReallyInteractiveFromUser() is always false because
             // gameObject.transform.position is not in the mesh. So IsReallyInteractiveFromUser() is true means
             // the position annotation work well
