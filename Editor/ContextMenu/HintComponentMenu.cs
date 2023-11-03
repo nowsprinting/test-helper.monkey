2ae217912d26b1d7c1bfa2c8b0f0319e0cb2cc83
Kuniwak <yuki.kokubun@dena.com>
2023-11-04 08:18:43 +0900

    Add to context menu
    Co-authored-by: Koji Hasegawa <hasegawa@nowsprinting.com>

---
+++ Editor/ContextMenu/HintComponentMenu.cs
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using TestHelper.Monkey;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.ContextMenu
+{
+    /// <summary>
+    /// Attaches interactive component hints
+    /// </summary>
+    public static class HintComponentMenu
+    {
+        [MenuItem("GameObject/TestHelper.Monkey/Interactive Component Hint Object")] // on Hierarchy window
+        private static void CreateInteractiveComponentHintObjectMenuItem()
+        {
+            new GameObject("Interactive Component Hint").AddComponent<InteractiveComponentHint>();
+        }
+    }
+}
