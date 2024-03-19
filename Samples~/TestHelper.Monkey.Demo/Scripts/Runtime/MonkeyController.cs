1d4a1bbce91c01aa528951cc598f4e7e3a94f76c
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-03-19 17:54:46 +0900

    Add demo app into Samples

---
+++ Samples~/TestHelper.Monkey.Demo/Scripts/Runtime/MonkeyController.cs
@@ -0,0 +1,70 @@
+// Copyright (c) 2023-2024 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestHelper.Monkey.Demo
+{
+    /// <summary>
+    /// Run/ Stop monkey testing.
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class MonkeyController : MonoBehaviour
+    {
+        public int lifetimeSeconds = 30;
+        public int delayMillis = 200;
+        public int secondsToErrorForNoInteractiveComponent = 5;
+        public int touchAndHoldDelayMillis = 1000;
+        public bool gizmos;
+        public bool screenshots;
+
+        private bool _running;
+        private Text _buttonLabel;
+        private CancellationTokenSource _cts;
+
+        private void Start()
+        {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(async () =>
+            {
+                await OnClick();
+            });
+            _buttonLabel = button.GetComponentInChildren<Text>();
+        }
+
+        private async Task OnClick()
+        {
+            if (_running)
+            {
+                _cts.Cancel();
+                _cts.Dispose();
+                _running = false;
+            }
+            else
+            {
+                var config = new MonkeyConfig
+                {
+                    Lifetime = TimeSpan.FromSeconds(lifetimeSeconds),
+                    DelayMillis = delayMillis,
+                    SecondsToErrorForNoInteractiveComponent = secondsToErrorForNoInteractiveComponent,
+                    TouchAndHoldDelayMillis = touchAndHoldDelayMillis,
+                    Gizmos = gizmos,
+                    Screenshots = screenshots ? new ScreenshotOptions() : null
+                };
+                _cts = new CancellationTokenSource();
+                _running = true;
+                await Monkey.Run(config, _cts.Token).SuppressCancellationThrow();
+                _running = false;
+            }
+        }
+
+        private void Update()
+        {
+            _buttonLabel.text = _running ? "Stop monkey testing" : "Run monkey testing";
+        }
+    }
+}
