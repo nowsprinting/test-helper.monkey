b0a5c5afcce123ce5ad5cbcd80fb5823a85aa294
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-03-20 06:41:22 +0900

    Fix reduce the execution time of lottery tests

--- Tests/Runtime/MonkeyTest.cs
+++ Tests/Runtime/MonkeyTest.cs
@@ -326,7 +326,7 @@ namespace TestHelper.Monkey
             var spyLogger = new SpyLogger();
             var config = new MonkeyConfig
             {
-                Lifetime = TimeSpan.FromSeconds(10), // 10sec
+                Lifetime = TimeSpan.FromSeconds(2), // 2sec
                 DelayMillis = 1, // 1ms
                 TouchAndHoldDelayMillis = 1, // 1ms
                 Random = new RandomWrapper(0), // pin seed
