8dedd0fa92b101ee45852fa8b815a80d7b80ee14
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-02-17 08:26:05 +0900

    Fix to exclude IgnoreAnnotation if disabled

---
+++ Tests/Runtime/DefaultStrategies/DefaultIgnoreStrategyTest.cs
@@ -0,0 +1,45 @@
+// Copyright (c) 2023-2025 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using NUnit.Framework;
+using TestHelper.Attributes;
+using TestHelper.Monkey.Annotations;
+using UnityEngine;
+
+namespace TestHelper.Monkey.DefaultStrategies
+{
+    [TestFixture]
+    public class DefaultIgnoreStrategyTest
+    {
+        private const string TestScene = "../../Scenes/PhysicsRaycasterSandbox.unity";
+
+        [Test]
+        [LoadScene(TestScene)]
+        public void IsIgnore_WithAnnotation_Ignored()
+        {
+            var cube = GameObject.Find("Cube");
+            cube.AddComponent<IgnoreAnnotation>();
+
+            Assert.That(DefaultIgnoreStrategy.IsIgnored(cube), Is.True);
+        }
+
+        [Test]
+        [LoadScene(TestScene)]
+        public void IsIgnore_WithDisabledAnnotation_NotIgnored()
+        {
+            var cube = GameObject.Find("Cube");
+            var annotation = cube.AddComponent<IgnoreAnnotation>();
+            annotation.enabled = false;
+
+            Assert.That(DefaultIgnoreStrategy.IsIgnored(cube), Is.False);
+        }
+
+        [Test]
+        [LoadScene(TestScene)]
+        public void IsIgnore_WithoutAnnotation_NotIgnored()
+        {
+            var cube = GameObject.Find("Cube");
+            Assert.That(DefaultIgnoreStrategy.IsIgnored(cube), Is.False);
+        }
+    }
+}
