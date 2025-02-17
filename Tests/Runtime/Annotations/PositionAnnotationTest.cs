609b486d911ef960fc125024ee0f8b8520b72cca
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-02-17 17:49:03 +0900

    Add on scaled canvas test cases

--- Tests/Runtime/Annotations/PositionAnnotationTest.cs
+++ Tests/Runtime/Annotations/PositionAnnotationTest.cs
@@ -1,7 +1,11 @@
 // Copyright (c) 2023-2025 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
 using NUnit.Framework;
 using TestHelper.Attributes;
 using TestHelper.Monkey.DefaultStrategies;
@@ -12,45 +16,65 @@ namespace TestHelper.Monkey.Annotations
     [TestFixture]
     public class PositionAnnotationTest
     {
-        private const string TestScene = "../../Scenes/PositionAnnotations.unity";
-
-        [Test]
-        [LoadScene(TestScene)]
-        public void AttachAnnotation_CorrectedToBeReachable(
-            [Values(
-                "WorldOffsetAnnotation",
-                "ScreenOffsetAnnotation",
-                "WorldPositionAnnotation",
-                "ScreenPositionAnnotation"
-            )]
-            string name
-        )
+        [TestFixture]
+        public class NotOnScaledCanvas
         {
-            var target = GameObject.Find(name);
+            private const string TestScene = "../../Scenes/PositionAnnotations.unity";
 
-            // Without no position annotations, IsReachable() is always false because
-            // gameObject.transform.position is not in the mesh. So IsReachable() is true means
-            // the position annotation work well
-            Assert.That(DefaultReachableStrategy.IsReachable(target), Is.True);
+            private static readonly IEnumerable<string> s_annotations = new[]
+            {
+                "ScreenOffsetAnnotation", "ScreenPositionAnnotation", "WorldOffsetAnnotation",
+                "WorldPositionAnnotation",
+            };
+
+            [TestCaseSource(nameof(s_annotations))]
+            [LoadScene(TestScene)]
+            public void AttachAnnotation_CorrectedToBeReachable(string name)
+            {
+                var target = GameObject.Find(name);
+
+                // Without no position annotations, IsReachable() is always false because
+                // gameObject.transform.position is not in the mesh. So IsReachable() is true means
+                // the position annotation work well
+                Assert.That(DefaultReachableStrategy.IsReachable(target), Is.True);
+            }
+
+            [TestCaseSource(nameof(s_annotations))]
+            [LoadScene(TestScene)]
+            public void DisabledAnnotation_NotReachable(string name)
+            {
+                var target = GameObject.Find(name);
+                var annotation = target.GetComponents<MonoBehaviour>().First(x => x.GetType().Name.Equals(name));
+                annotation.enabled = false;
+
+                Assert.That(DefaultReachableStrategy.IsReachable(target), Is.False);
+            }
         }
 
-        [Test]
-        [LoadScene(TestScene)]
-        public void DisabledAnnotation_NotReachable(
-            [Values(
-                "WorldOffsetAnnotation",
-                "ScreenOffsetAnnotation",
-                "WorldPositionAnnotation",
-                "ScreenPositionAnnotation"
-            )]
-            string name
-        )
+        [TestFixture]
+        public class OnScaledCanvas
         {
-            var target = GameObject.Find(name);
-            var annotation = target.GetComponents<MonoBehaviour>().First(x => x.GetType().Name.Equals(name));
-            annotation.enabled = false;
+            private const string TestScene = "../../Scenes/ScaledCanvas.unity";
 
-            Assert.That(DefaultReachableStrategy.IsReachable(target), Is.False);
+            private static readonly IEnumerable<string> s_annotations = new[]
+            {
+                "ScreenOffsetAnnotation", // The offset is within the GameObject's rect on XGA
+                "ScreenPositionAnnotation", // The position is within the GameObject's rect on XGA
+            };
+
+            [TestCaseSource(nameof(s_annotations))]
+            [LoadScene(TestScene)]
+            [CanBeNull]
+            public async Task AttachAnnotation_OnScaledCanvas_CorrectedToBeReachable(string name)
+            {
+                await UniTask.NextFrame();
+                var target = GameObject.Find(name);
+
+                Assert.That(DefaultReachableStrategy.IsReachable(target, verboseLogger: Debug.unityLogger), Is.True);
+                // The offset/position is within the GameObject's rect on XGA display.
+                // If the annotation's properties don't consider CanvasScaler, the specified position will be outside the GameObject's rect on VGA display.
+                // Then IsReachable will return false.
+            }
         }
     }
 }
