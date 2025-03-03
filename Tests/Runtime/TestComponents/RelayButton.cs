05d56bb7d0f91ea3eda843b371f691d158837b7a
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-03-03 09:37:36 +0900

    Add infinite loop test scene

---
+++ Tests/Runtime/TestComponents/RelayButton.cs
@@ -0,0 +1,36 @@
+// Copyright (c) 2023-2025 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestHelper.Monkey.TestComponents
+{
+    [RequireComponent(typeof(Button))]
+    // [AddComponentMenu("/")] // Hide from "Add Component" picker
+    public class RelayButton : MonoBehaviour
+    {
+        [field: SerializeField]
+        [Tooltip("Next enabled button.")]
+        public Button NextButton { get; private set; }
+
+        [field: SerializeField]
+        [Tooltip("Enabled on load. It is assumed that only one is set to true in the scene..")]
+        public bool EnabledOnLoad { get; set; } = false;
+
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.interactable = EnabledOnLoad;
+            _button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            _button.interactable = false;
+            NextButton.interactable = true;
+        }
+    }
+}
